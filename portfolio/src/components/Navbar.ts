6e1a4b297d86a8ed503b9d60b9bbfdcdf2da35a7
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-08-13 17:02:09 -0300

    Estruturação

---
+++ portfolio/src/components/Navbar.ts
@@ -0,0 +1,26 @@
+// src/components/Navbar.js
+import React from "react";
+import { Link } from "react-router-dom"; 
+
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-2 mt-0 w-full">
+      <div className="container mx-auto flex items-center justify-between">
+        <Link to="/">
+          <div className="text-lg font-bold text-white">Jonathan Sena</div>
+        </Link>
+        <div className="flex items-center">
+          <Link to="/about">
+            <div className="text-lg font-bold text-white mr-6">About</div>
+          </Link>
+          <Link to="/projects">
+            <div className="text-lg font-bold text-white mr-6">Projects</div>
+          </Link>
+          <Link to="/contact">
+            <div className="text-lg font-bold text-white">Contact</div>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
