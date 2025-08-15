513981e96933601b51608660ffd71db163cfbd27
Jonathan Sena da Silva <jonathansilvagw@gmail.com>
2025-08-15 16:29:36 -0300

    Navegação base

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,14 +1,14 @@
-// src/App.js
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
 import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- portfolio/src/pages/Home.tsx
+++ portfolio/src/pages/Home.tsx
@@ -1,9 +1,6 @@
-import Navbar from "../components/Navbar";
-
-export default function Projects() {
+export default function Home() {
     return (
         <div>
-            <Navbar />
             <h1>Home</h1>
             <p>This is the home page.</p>
         </div>
