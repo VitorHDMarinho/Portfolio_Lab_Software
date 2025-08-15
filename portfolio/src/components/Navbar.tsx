9c09babb362d839d482ba6640aba88b53266b750
VitorHDMarinho <vitorhdmarinho@gmail.com>
2025-08-15 19:37:54 -0300

    Navbar

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -1,50 +1,34 @@
 import type { FC } from "react";
 import type { JSX } from "react";
 import { Link } from "react-router-dom";
+import "../styles/Navbar.css"
+import perfilImg from "../images/perfil.png"; // importa a imagem
 
-const navStyles: React.CSSProperties = {
-  backgroundColor: "#2d3748",
-  padding: "8px",
-  marginTop: "0",
-  width: "100%",
-};
 
-const containerStyles: React.CSSProperties = {
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
-  maxWidth: "1200px",
-  margin: "0 auto",
-};
-
-const linkStyles: React.CSSProperties = {
-  fontSize: "1.125rem",
-  fontWeight: "bold",
-  color: "white",
-  textDecoration: "none",
-  marginRight: "1.5rem",
-};
 
 const Navbar: FC = (): JSX.Element => {
   return (
-    <nav style={navStyles}>
-      <div style={containerStyles}>
-        <Link to="/" style={{ ...linkStyles, marginRight: 0 }}>
-          Jonathan Sena
+    <nav className="navStyles">
+      <div className="linksNavStyle">
+      <div className="containerStyles">
+        <Link to="/" className="linkStyles">
+          Vitor Marinho
         </Link>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <Link to="/about" style={linkStyles}>
+          <Link to="/about" className="linkStyles">
             About
           </Link>
-          <Link to="/projects" style={linkStyles}>
+          <Link to="/projects" className="linkStyles">
             Projects
           </Link>
-          <Link to="/contact" style={{ ...linkStyles, marginRight: 0 }}>
+          <Link to="/contact" className="linkStyles">
             Contact
           </Link>
-        </div>
       </div>
-    </nav>
+    </div>
+      <img  className = "imgStyles" src={perfilImg} alt="" />
+  </nav>
+
+    
   );
 };
 
